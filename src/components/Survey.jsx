ea357b278724274b93a33814da5ccd2afb660de7
Julian Ng <julesnuggy@gmail.com>
2023-10-27 16:15:59 +0100

    Update styles to match new component name

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -4,12 +4,12 @@ function Survey() {
   const [open, setOpen] = useState(false); //Ignore this state
 
   return (
-    <main className="main">
-      <section className={`main__list ${open ? "open" : ""}`}>
+    <main className="survey">
+      <section className={`survey__list ${open ? "open" : ""}`}>
         <h2>Answers list</h2>
         {/* answers should go here */}
       </section>
-      <section className="main__form">{/* a form should be here */}</section>
+      <section className="survey__form">{/* a form should be here */}</section>
     </main>
   );
 }
