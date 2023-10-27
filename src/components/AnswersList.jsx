22c987baff2fe968405e2fe3bbf1b10d20e7c601
Julian Ng <julesnuggy@gmail.com>
2023-10-27 16:09:32 +0100

    Add components

---
+++ src/components/AnswersList.jsx
@@ -0,0 +1,15 @@
+import AnswersItem from "./AnswersItem";
+
+export default function AnswersList(props) {
+  console.log("Inside AnswersList: ", props);
+
+  const { answersList } = props;
+
+  return (
+    <ul>
+      {answersList.map((answerItem, i) => (
+        <AnswersItem answerItem={answerItem} key={i} />
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Main() {
+  const [open, setOpen] = useState(false); //Ignore this state
+
+  return (
+    <main className="main">
+      <section className={`main__list ${open ? "open" : ""}`}>
+        <h2>Answers list</h2>
+        {/* answers should go here */}
+      </section>
+      <section className="main__form">{/* a form should be here */}</section>
+    </main>
+  );
+}
+
+export default Main;
